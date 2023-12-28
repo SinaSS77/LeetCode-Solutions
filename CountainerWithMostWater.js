65ec7dc3059057ffe21b532c2e85ff9c7548faf1
Sina Molaverdikhani <s.molaverdikhani@gmail.com>
2023-12-27 17:48:20 -0800

    Second question

---
+++ CountainerWithMostWater.js
@@ -0,0 +1,9 @@
+/* You are given an array of positive integers where each integer represents the height on a vertical line of a chart. Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.*/
+
+// Step 1: Barriers
+// Does the thikness of the lines affect the area? NO
+// Do the left and right side of the graph counts as walls? NO
+// Does a higher line inside a container affect the area? No
+// 
