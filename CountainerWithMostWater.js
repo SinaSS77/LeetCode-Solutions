a81214dc5715b6621cbeac8f6c3ae0bd6bc18f43
Sina Molaverdikhani <s.molaverdikhani@gmail.com>
2024-01-20 18:27:38 -0800

    Container with most water

--- CountainerWithMostWater.js
+++ CountainerWithMostWater.js
@@ -7,3 +7,47 @@ Return the maximum amount of water a container can store.*/
 // Do the left and right side of the graph counts as walls? NO
 // Does a higher line inside a container affect the area? No
 // 
+// Step 2: Brute force solution
+const heightsArray1 = [7,1,2,3,9]
+const getMaxWaterContainerBr = function(heights){
+  let maxArea = 0;
+  for (let p1 = 0 ; p1<heights.length; p1++){
+    for (let p2=p1+1; p2 < heights.length; p2++){
+      const height = Math.min(heights[p1], heights[p2]);
+      const width = p2-p1;
+      const area = height*width;
+      maxArea = Math.max(maxArea, area);
+    }
+  };
+  return maxArea;
+}
+
+console.log(getMaxWaterContainerBr(heightsArray1));
+
+// step 3: Optimal Solution: Two point solution (Shifting pointers)
+
+const heightsArray2 = [4,8,1,2,3,9];
+
+const getMaxWaterContainerOp = function(heights){
+  let p1 =0, p2 = heights.length-1 ; maxArea = 0;
+  console.log({p1 , p2 , maxArea});
+
+  while(p1<p2){
+    console.log({p1 , p2});
+    const height = Math.min(heights[p1], heights[p2]);
+    const width = p2-p1;
+    const area = height*width;
+    console.log(`height:${height}, width:${width}, area:${area}`);
+    maxArea = Math.max(maxArea, area);
+    console.log({maxArea});
+
+    if (heights[p1] <= heights[p2]){
+      p1++;
+    } else {
+      p2--;
+    } 
+  }
+  return maxArea;
+}
+
+console.log(getMaxWaterContainerOp(heightsArray2));
